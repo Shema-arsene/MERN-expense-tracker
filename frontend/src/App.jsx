e91f579fb81c3c01fcad704d324a876585d037dd
Shema Arsene <aniyonkurushema@gmail.com>
2025-06-06 09:33:26 +0200

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-center font-bold capitalize my-40">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
