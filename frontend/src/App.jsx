edf7d7dda870657d0a504b0eab6c824fea8e3ad5
Shema Arsene <aniyonkurushema@gmail.com>
2025-06-06 10:05:14 +0200

    Project setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,47 @@
+import React from "react"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom"
+
+// Auth
+import Login from "./pages/Auth/Login.jsx"
+import Signup from "./pages/Auth/Signup.jsx"
+
+// Dashboard
+import Home from "./pages/Dashboard/Home.jsx"
+import Income from "./pages/Dashboard/Income.jsx"
+import Expenses from "./pages/Dashboard/Expenses.jsx"
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl text-center font-bold capitalize my-40">
-        Hello world!
-      </h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/Signup" exact element={<Signup />} />
+          <Route path="/dashboard" exact element={<Home />} />
+          <Route path="/income" exact element={<Income />} />
+          <Route path="/expenses" exact element={<Expenses />} />
+        </Routes>
+      </Router>
     </>
   )
 }
 
 export default App
+
+const Root = () => {
+  // Check if token exists in localstorage
+  const isAuthenticated = !!localStorage.getItem("token")
+
+  // Redirect to dashboard if suthenticated, otherwise login
+  return isAuthenticated ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to="/login" />
+  )
+}
--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,7 +1,16 @@
 import React from "react"
 
 const Login = () => {
-  return <div>Login</div>
+  return (
+    <section>
+      <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
+        <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
+        <p className="text-xs text-slate-700 mt-[5px] mb-6">
+          Please enter your details to login
+        </p>
+      </div>
+    </section>
+  )
 }
 
 export default Login
